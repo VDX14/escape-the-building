46e68e9614dbe91ef66ecaf70de3def0643bb13c
VDX14 <vanessa.xiong1@mymail.nwtc.edu>
2026-02-05 19:43:37 -0600

    Added welcome message and scenario to Game.java

--- src/game/Game.java
+++ src/game/Game.java
@@ -15,6 +15,18 @@ public class Game {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Prints welcome message to console 
+		System.out.println("===================================");
+		System.out.println("        ESCAPE THE BUILDING        ");
+		System.out.println("===================================");
+		System.out.println("You wake up in a strange building.");
+		System.out.println("You must explore and find items that will help you survive and escape.");
+		System.out.println("There is also an enemy roaming around. Be prepare to fight if approached.");
+		System.out.println();
+		System.out.println("Type 'help' to see available commands.");
+		System.out.println("Good luck... you will need it!");
+		System.out.println("===================================");
+		
 		//Scanner used to read player input from console.
 		Scanner scanner = new Scanner(System.in);
 		
