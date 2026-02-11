db8c59525b690c8d7725598e13d3f4f113789fb0
VDX14 <vanessa.xiong1@mymail.nwtc.edu>
2026-02-11 16:30:24 -0600

    Added assertion for player Updated try-catch

--- src/game/Game.java
+++ src/game/Game.java
@@ -22,146 +22,141 @@ public class Game {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Scanner declared outside of try so it can be accessed. 
-		Scanner scanner = null;
-		
+		//Immediate try-catch at the start. 
 		try {
 		
-		//Prints welcome message to console. 
-		System.out.println("===================================");
-		System.out.println("        ESCAPE THE BUILDING        ");
-		System.out.println("===================================");
-		System.out.println("You wake up in a strange building.");
-		System.out.println("You must explore and find items that will help you survive and escape.");
-		System.out.println("There is also an enemy roaming around. Be prepare to fight if approached.");
-		System.out.println();
-		System.out.println("Type 'help' to see available commands.");
-		System.out.println("Good luck... you will need it!");
-		System.out.println("===================================");
-		
-		//Initialize Game World.
-		//Singleton of Game world. 
-		GameWorld world = GameWorld.getInstance();
-		//build all rooms.
-		world.buildWorld();
-		
-		//Initialize player.
-		Player player = new Player();
-		player.setName("Elite");
-		player.setHealth(100);
-		player.move(world.getStartingRoom());
-		
-		//Initialize logger.
-		GameLogger logger = GameLogger.getInstance();
-		logger.log(player.getName() + " enters the building");
-		
-		//Initialize command parser.
-		SimpleCommandParser parser = new SimpleCommandParser();
-		
-		//Main game input loop.
-		//Scanner used to read player input from console.
-		scanner = new Scanner(System.in);
-		boolean playing = true;
-		
-		//Loop repeats as long as player is true.
-		while (playing) {
-			//Show prompt so player knows to type something.
-			System.out.print("> ");
-			//Reads input from user and removes extra spaces.
-			String input = scanner.nextLine().trim();
+			//Try-with-resource for scanner 
+			try (Scanner scanner = new Scanner(System.in)) {
 			
-			//Logs what user types 
-			logger.log("Player typed command: " + input);
+			//Prints welcome message to console. 
+			System.out.println("===================================");
+			System.out.println("        ESCAPE THE BUILDING        ");
+			System.out.println("===================================");
+			System.out.println("You wake up in a strange building.");
+			System.out.println("You must explore and find items that will help you survive and escape.");
+			System.out.println("There is also an enemy roaming around. Be prepare to fight if approached.");
+			System.out.println();
+			System.out.println("Type 'help' to see available commands.");
+			System.out.println("Good luck... you will need it!");
+			System.out.println("===================================");
 			
-			//Skips empty inputs.
-			if (input.isEmpty()) {
-				continue;
-			}
+			//Initialize Game World.
+			//Singleton of Game world. 
+			GameWorld world = GameWorld.getInstance();
+			//build all rooms.
+			world.buildWorld();
 			
-			//The parse commands. 
-			Command command = parser.parse(input);
-			String verb = command.getVerb();
-			String noun = command.getNoun();
+			//Initialize player.
+			Player player = new Player();
+			assert player != null : "Player can't be null";
+			player.setName("Elite");
+			player.setHealth(100);
+			player.move(world.getStartingRoom());
 			
-			//Create default demands when "help" is typed. 
-			if (verb.equalsIgnoreCase("help")) {
+			
+			//Initialize logger.
+			GameLogger logger = GameLogger.getInstance();
+			logger.log(player.getName() + " enters the building");
+			
+			//Initialize command parser.
+			SimpleCommandParser parser = new SimpleCommandParser();
+			
+			//Main game input loop.
+			boolean playing = true;
+			
+			//Loop repeats as long as player is true.
+			while (playing) {
+				//Show prompt so player knows to type something.
+				System.out.print("> ");
+				//Reads input from user and removes extra spaces.
+				String input = scanner.nextLine().trim();
 				
-				 System.out.println("Default commands:");
-				 System.out.println("look       - Examine the current room");
-				 System.out.println("inventory  - Show your items");
-				 System.out.println("go [dir]   - Move in a direction (north, south, east, west)");
-				 System.out.println("use [item] - Use an item from your inventory");
-				 System.out.println("quit       - Exit the game");	
-			}
-			
-			//Look command, which shows info about the current room.
-			else if (verb.equalsIgnoreCase("look")) {
-			
-				 System.out.println(player.getCurrentRoom().describe());
-			}
-			
-			//Inventory command, which shows items from player's inventory
-			else if (verb.equalsIgnoreCase("inventory")) {
+				//Logs what user types 
+				logger.log("Player typed command: " + input);
 				
-				if (player.getInventory().isEmpty()) {
-			        // Inventory has no items
-			        System.out.println("Your inventory is empty.");
-			    } else {
-			        // Inventory contains items
-			        System.out.println("You have:");
-			        for (Item item : player.getInventory()) {
-			            System.out.println("- " + item.getName());
-			        }
-			    }
-			}
-			
-			//Go command, which moves player (not fully implemented yet!)
-			else if (verb.equalsIgnoreCase("go")) {
-			
-				System.out.println("Using " + noun + "(usuage not fully implemented yet.)");
-			}
-	
-			//Quit command, which ends the game loop.
-			else if (verb.equalsIgnoreCase("quit")) {
+				//Skips empty inputs.
+				if (input.isEmpty()) {
+					continue;
+				}
 				
-				System.out.println("Thanks for playing!");
-				playing = false;
-			}
-			
-			//Default command for unrecognizable commands.
-			else {
+				//The parse commands. 
+				Command command = parser.parse(input);
+				String verb = command.getVerb();
+				String noun = command.getNoun();
 				
-				System.out.println("Unknown command. Type 'help' for a list of commands.");
-			}
+				//Create default demands when "help" is typed. 
+				if (verb.equalsIgnoreCase("help")) {
+					
+					 System.out.println("Default commands:");
+					 System.out.println("look       - Examine the current room");
+					 System.out.println("inventory  - Show your items");
+					 System.out.println("go [dir]   - Move in a direction (north, south, east, west)");
+					 System.out.println("use [item] - Use an item from your inventory");
+					 System.out.println("quit       - Exit the game");	
+				}
+				
+				//Look command, which shows info about the current room.
+				else if (verb.equalsIgnoreCase("look")) {
+				
+					 System.out.println(player.getCurrentRoom().describe());
+				}
+				
+				//Inventory command, which shows items from player's inventory
+				else if (verb.equalsIgnoreCase("inventory")) {
+					
+					if (player.getInventory().isEmpty()) {
+				        // Inventory has no items
+				        System.out.println("Your inventory is empty.");
+				    } else {
+				        // Inventory contains items
+				        System.out.println("You have:");
+				        for (Item item : player.getInventory()) {
+				            System.out.println("- " + item.getName());
+				        }
+				    }
+				}
+				
+				//Go command, which moves player (not fully implemented yet!)
+				else if (verb.equalsIgnoreCase("go")) {
+				
+					System.out.println("Using " + noun + " (usuage not fully implemented yet.)");
+				}
+		
+				//Quit command, which ends the game loop.
+				else if (verb.equalsIgnoreCase("quit")) {
+					
+					System.out.println("Thanks for playing!");
+					playing = false;
+				}
+				
+				//Default command for unrecognizable commands.
+				else {
+					
+					System.out.println("Unknown command. Type 'help' for a list of commands.");
+				}
+				
+				//Check if the player's health has dropped to 0 or below. 
+				if (!player.isAlive()) {
+					System.out.println("You have died. Game over!");
+					//Log the death event 
+					logger.log(player.getName() + " has died.");
+					playing = false;
+				}
+			} //end while loop
 			
-			//Check if the player's health has dropped to 0 or below. 
-			if (!player.isAlive()) {
-				System.out.println("You have died. Game over!");
-				//Log the death event 
-				logger.log(player.getName() + " has died.");
-				playing = false;
-			}
+			//Log game session that it has ended.
+			logger.log("Game session ended.");
+			
 		}
-		
-		//Log game session that it has ended.
-		logger.log("Game session ended.");
-	
-		
+			
 	} catch (Exception e) {
-		
-		System.out.println("An unexpected error occured.");
-		System.out.println("The game will close now!");
-		
-		//Print error for developer debugging.
-		e.printStackTrace();
-	
-	}finally {
-		
-		if (scanner != null ) {
-			scanner.close();
+			
+			System.out.println("An unexpected error occured.");
+			System.out.println("The game will close now!");
+			e.printStackTrace();
 		}
-		
+			
 		System.out.println("Game Closed.");
+		
 	}
-}
 }
